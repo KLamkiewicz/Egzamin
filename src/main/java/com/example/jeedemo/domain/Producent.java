2de435a637ff3d7bfc17a78d47fa044c1036d1dd
klamkiewicz <icespam@wp.pl>
2014-02-01 22:42:08 +0100

    Init

---
+++ src/main/java/com/example/jeedemo/domain/Producent.java
@@ -0,0 +1,43 @@
+package com.example.jeedemo.domain;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.NamedQuery;
+
+@Entity
+@NamedQuery(name ="producent.getProducenci" , query = "Select p FROM Producent p where p.deleted = false ")
+public class Producent {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	private String nazwa;
+	private boolean deleted = false;
+	
+	public boolean isDeleted() {
+		return deleted;
+	}
+
+	public void setDeleted(boolean deleted) {
+		this.deleted = deleted;
+	}
+
+	public Long getId() {
+		return id;
+	}
+	
+	public void setId(Long id) {
+		this.id = id;
+	}
+	
+	public String getNazwa() {
+		return nazwa;
+	}
+	
+	public void setNazwa(String nazwa) {
+		this.nazwa = nazwa;
+	}
+		
+}
